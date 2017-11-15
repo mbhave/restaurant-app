6ab6e31e3dbec6bd3561ebb25a840915d5ed8a96
Stephane Nicoll <snicoll@pivotal.io>
2017-11-15 13:32:17 +0100

    Flux handling demonstration
    Let's assume a RestaurantLookupService that returns all the restaurants
    in our area. The filter operator on Reactor allows us to implement the
    requested feature but unfortunately our test does not start anymore as
    it uses slicing.
    
    We can mock the lookup service and returns sample data. But we still
    haven't a way to get those restaurants and getting them all is too
    limited.

--- src/main/java/com/example/restaurantapp/web/RestaurantController.java
+++ src/main/java/com/example/restaurantapp/web/RestaurantController.java
@@ -1,9 +1,28 @@
 package com.example.restaurantapp.web;
 
+import com.example.restaurantapp.domain.Restaurant;
+import com.example.restaurantapp.domain.RestaurantRepository;
+import reactor.core.publisher.Flux;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestaurantController {
 
+	private final RestaurantRepository repository;
+
+	public RestaurantController(RestaurantRepository repository) {
+		this.repository = repository;
+	}
+
+	@GetMapping("/restaurants")
+	public Flux<Restaurant> getByCategoryAndPrice(@RequestParam String category,
+			@RequestParam Double maxPrice) {
+		return this.repository.findAll()
+				.filter(restaurant -> category.equals(restaurant.getCategory()))
+				.filter(restaurant -> restaurant.getPricePerPerson() < maxPrice);
+	}
 
 }
--- src/test/java/com/example/restaurantapp/web/RestaurantControllerTests.java
+++ src/test/java/com/example/restaurantapp/web/RestaurantControllerTests.java
@@ -1,12 +1,20 @@
 package com.example.restaurantapp.web;
 
+import java.util.Set;
+
+import com.example.restaurantapp.domain.Restaurant;
+import com.example.restaurantapp.domain.RestaurantRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import reactor.core.publisher.Flux;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest(RestaurantController.class)
@@ -15,8 +23,12 @@ public class RestaurantControllerTests {
 	@Autowired
 	private WebTestClient webTestClient;
 
+	@MockBean
+	private RestaurantRepository repository;
+
 	@Test
 	public void findCheapSushiPlaces() {
+		given(repository.findAll()).willReturn(sampleRestaurants());
 		this.webTestClient.get()
 				.uri("/restaurants?category={c}&maxPrice={s}", "sushi", 20.00)
 				.exchange()
@@ -24,6 +36,16 @@ public class RestaurantControllerTests {
 				.expectBody()
 				.jsonPath("$[0].name").isEqualTo("Sushi2Go")
 				.jsonPath("$.length()", 1);
+	}
+
+	private static Flux<Restaurant> sampleRestaurants() {
+		return Flux.fromIterable(Set.of(
+				new Restaurant("Sushi2Go", 15.00, "sushi"),
+				new Restaurant("Burger Spring", 11.00, "fast-food"),
+				new Restaurant("DoMONOs", 10.00, "fast-food"),
+				new Restaurant("Cheesecake ProxyFactory", 9.50, "pastry"),
+				new Restaurant("Sushi Heaven", 75.00, "sushi")
+		));
 	}
 
 }
