d1b7d17154521118a39a84bc23113870f2781fb8
Stephane Nicoll <snicoll@pivotal.io>
2017-11-15 14:28:36 +0100

    Remote service demonstration
    Let's assume that we have a reservation service running on port 8081.
    
    We want to get the first 3 restaurants that are available. Reactor helps
    us to combine data with a HTTP remote call to check availability, and
    the take operation limits the requests transparently for us.
    
    The remote service is slow and unreliable but it doesn't matter and we
    can stream the response as it comes, try:
    
    http :8080/restaurants/available --stream

--- src/main/java/com/example/restaurantapp/web/RestaurantController.java
+++ src/main/java/com/example/restaurantapp/web/RestaurantController.java
@@ -1,26 +1,48 @@
 package com.example.restaurantapp.web;
 
 import com.example.restaurantapp.domain.Restaurant;
+import com.example.restaurantapp.domain.RestaurantAvailability;
 import com.example.restaurantapp.domain.RestaurantRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class RestaurantController {
 
 	private final RestaurantRepository repository;
 
-	public RestaurantController(RestaurantRepository repository) {
+	private final WebClient webClient;
+
+	public RestaurantController(RestaurantRepository repository,
+			WebClient.Builder webClientBuilder) {
 		this.repository = repository;
+		this.webClient = webClientBuilder.baseUrl("http://localhost:8081").build();
 	}
 
 	@GetMapping("/restaurants")
 	public Flux<Restaurant> getByCategoryAndPrice(@RequestParam String category,
 			@RequestParam Double maxPrice) {
 		return this.repository.findByCategoryAndPricePerPersonLessThan(category, maxPrice);
+	}
+
+	@GetMapping(path = "/restaurants/available", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+	public Flux<RestaurantAvailability> getAvailableRestaurants() {
+		return this.repository.findAll()
+				.flatMap(this::getAvailability)
+				.filter(RestaurantAvailability::isAvailable)
+				.take(3);
+	}
+
+	private Mono<RestaurantAvailability> getAvailability(Restaurant restaurant) {
+		return this.webClient.get()
+				.uri("/restaurants/{name}/availability", restaurant.getName())
+				.retrieve().bodyToMono(RestaurantAvailability.class);
 	}
 
 }
--- src/test/java/com/example/restaurantapp/web/RestaurantControllerTests.java
+++ src/test/java/com/example/restaurantapp/web/RestaurantControllerTests.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import reactor.core.publisher.Flux;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,6 +17,7 @@ import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest(RestaurantController.class)
+@AutoConfigureWebClient // TODO: we should probably move the client stuff to a separate service in the "real" repo
 public class RestaurantControllerTests {
 
 	@Autowired
